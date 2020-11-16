a0a34ed29ebb67052783c6f8a3352e495ff596d1
caioalef1 <65835881+caioalef1@users.noreply.github.com>
2020-11-16 06:19:25 -0300

    implementações
    correção do bug que não salva idade e email
    não consegui usar o sweetalert.

--- index.php
+++ index.php
@@ -8,24 +8,28 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 
-    <title>Hello, world!</title>
+    <title>Teste Formulário</title>
   </head>
   <body>
     <div class='container'>
-    <form action="conexao.php" method="post">
+    <form action="requisicao.php" method="post">
     <div class="form-group">
       <label for="exampleInputEmail1">Seu nome</label>
-      <input type="text" class="form-control" name='name'>
+      <input type="text" class="form-control" name='name' placeholder="Ex: Caio Alef">
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1">Sua idade</label>
-      <input type="text" class="form-control" name="idade">
+      <input type="number" class="form-control" name="age" placeholder="Ex: 26">
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">CIdade que você mora</label>
-      <input type="text" class="form-control" name="cidade">
+      <label for="exampleInputEmail1" >Cidade que você mora</label>
+      <input type="text" class="form-control" name="city" placeholder="Ex: Feira de Santana">
     </div>
-    <button type="submit" class="btn btn-primary" name="enviar">Enviar</button>
+    <div class="form-group">
+      <label for="exampleInputEmail1" >Email</label>
+      <input type="email" class="form-control" name="email" placeholder="Ex: caioalefonline@gmail.com">
+    </div>
+    <button type="submit" class="btn btn-secondary" name="enviar">Enviar</button>
   </form>
   </div>
 
@@ -35,11 +39,7 @@
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"> </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    
   </body>
 </html>
 
-<?php
-if(isset($_POST["enviar"])){
-echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
-}
-?>
--- requisicao.php
+++ requisicao.php
@@ -1,9 +1,23 @@
 <?php include_once'conn.php';?>
 <html lang="pt">
 <body>
-    $name = $_POST["name"]
-    $age = $_POST["age"]
-    $city = $_POST["city"]
+<?php
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $city = $_POST["city"];
+    $email = $_POST["email"];
     $conn = mysqli_connect($server,$user,$password,$data_base); 
+
+    mysqli_select_db($conn,'$data_base');
+    $sql = "INSERT INTO cadastro(name,AGE,CITY,EMAIL) VALUES ('$name', '$age', '$city', '$email')";
+
+    if(mysqli_query($conn,$sql)){
+        echo "<script>alert('Dados gravados!'); window.location = 'index.php';</script>";
+    }else{
+        echo "Azedou" . sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+
+?>
 </body>
 </html>
